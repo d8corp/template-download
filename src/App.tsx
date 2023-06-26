d64ab3e96b9f9d35387acdc0cd23ce45972a3704
Mike Lysikov <d8corp@mail.ru>
2023-06-26 10:59:34 +0300

    add empty download check

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-export function download(url: string, fileName?: string) {
+export function download(url: string, fileName = '') {
   const link = document.createElement('a');
   document.body.appendChild(link);
 
-  if (fileName) {
-    link.download = fileName;
-  }
-
+  link.download = fileName;
   link.href = url;
   link.click();
   document.body.removeChild(link);
 }
 
 export const App = () => (
-  <button onclick={() => download('/example.png', 'test.png')}>download</button>
+  <div style={{display: 'flex', gap: '16px'}}>
+    <button onclick={() => download('/example.png', 'test.png')}>download test.png</button>
+    <button onclick={() => download('/example.png')}>download empty name</button>
+  </div>
 )
